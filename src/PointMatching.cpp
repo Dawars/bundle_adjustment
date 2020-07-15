46c5ae452e355ef6fa204c3150f77958306589b9
Dávid Komorowicz <dawars00@gmail.com>
2020-07-15 17:45:29 +0200

    Fix opencv mat dimension order

--- src/PointMatching.cpp
+++ src/PointMatching.cpp
@@ -48,8 +48,8 @@ void OnlinePointMatcher::matchKeypoints(std::vector<cv::Mat> & depthImages, Eige
         auto &kps = this->keypoints[i];
         auto num_current_points = kps.size();
 
-        const int frame_width = depthImages[i].size[0];
-        const int frame_height = depthImages[i].size[1];
+        const int frame_width = depthImages[i].size[1];
+        const int frame_height = depthImages[i].size[0];
 
         // build x, y, z observations
         for(int j=0; j<num_current_points; j++) {
@@ -58,10 +58,11 @@ void OnlinePointMatcher::matchKeypoints(std::vector<cv::Mat> & depthImages, Eige
 
             Eigen::Vector3f image_point;
             image_point << x_obs, y_obs, 1;
-            x_obs = clamp(x_obs, 0, frame_width);
-            y_obs = clamp(y_obs, 0, frame_height);
 
-            double depth = depthImages[i].at<double>(x_obs, y_obs);
+            assert(x_obs <= frame_width);
+            assert(y_obs <= frame_height);
+
+            double depth = depthImages[i].at<double>(y_obs, x_obs);
             Eigen::Vector3f cameraLine = instrinsicsInv * image_point;
             Eigen::Vector4f cameraPoint; // Andrew: don't think homogenous will be necessary
             cameraPoint << depth * cameraLine, 1;
